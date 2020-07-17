72c2812f657984b478e889dfb24ab505436b6767
Nicholas Bardy <nicholasbardy@gmail.com>
2020-07-16 18:52:36 -0700

    add forward slash check

--- wandb/data_types.py
+++ wandb/data_types.py
@@ -614,7 +614,8 @@ class Molecule(BatchableMedia):
         jsons = [obj.to_json(run) for obj in molecule_list]
 
         for obj in jsons:
-            if not obj['path'].startswith(cls.get_media_subdir()):
+            expected = util.to_forward_slash_path(cls.get_media_subdir())
+            if not obj['path'].startswith(expected):
                 raise ValueError('Files in an array of Molecule\'s must be in the {} directory, not {}'.format(
                     cls.get_media_subdir(), obj['path']))
 
