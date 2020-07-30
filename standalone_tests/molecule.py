a4b7ad4c1afd173b3a72c99e12e168f4d7012803
Nicholas Bardy <nicholasbardy@gmail.com>
2020-07-29 19:17:28 -0700

    change to smaller file for molecule

--- standalone_tests/molecule.py
+++ standalone_tests/molecule.py
@@ -3,7 +3,7 @@ import wandb
 N = 1
 
 all_tests = {
-    "6vw1": wandb.Molecule(open("../tests/fixtures/6vw1.pdb"),
+    "6vw1": wandb.Molecule(open("../tests/fixtures/3EVP.pdb"),
         caption="Structure of 2019-nCoV chimeric receptor-binding domain complexed with its receptor human ACE2")
 }
 
