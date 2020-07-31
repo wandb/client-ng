bdf0e12588d7a2c0ff309c716d6001ac809c0dd9
Nicholas Bardy <nicholasbardy@gmail.com>
2020-07-31 14:14:09 -0700

    Make run with pytest

---
+++ integration_tests/test_all_media.py
@@ -0,0 +1,17 @@
+import wandb
+from integration_tests.test_scripts.media.all_media_types import run_tests, all_tests
+
+def test_all_media():
+    api = wandb.Api()
+    run_path = run_tests()
+    # run_path = "all-media-test/runs/lbabj5x4"
+    run = api.run("local/" + run_path)
+
+    history = run.history()
+
+    for index, row in history.iterrows():
+        # print(row)
+        for k, v in all_tests.items():
+            history_object = row[k]
+            print("asset row" + k)
+            assert history_object != None
