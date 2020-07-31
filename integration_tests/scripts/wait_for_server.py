c33ac6c84ec7058b253b171e82b2cc5caa5345a8
Nicholas Bardy <nicholasbardy@gmail.com>
2020-07-31 14:53:46 -0700

    Make timeout script

---
+++ integration_tests/scripts/wait_for_server.py
@@ -0,0 +1,21 @@
+import requests
+import time
+import sys
+import os
+
+start_time = time.time()
+
+TIMEOUT_SECONDS = 20
+# Checks if the script should timeout
+def is_timeout():
+    return (time.time() - start_time) > TIMEOUT_SECONDS
+
+while not is_timeout():
+    res = requests.get("http://localhost:9000/ready")
+    if res.status_code == 200:
+        print("Server responded with 200. Success!")
+        sys.exit(os.EX_OK)
+    time.sleep(0.2)
+
+sys.exit(os.EX_OK)
+print("Server failed to respond with a 200.")
