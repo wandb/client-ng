ec7d53d44ff7fad8563937e2e41fab89f2cccf5d
Nicholas Bardy <nicholasbardy@gmail.com>
2020-07-31 15:11:06 -0700

    redo ping script

--- integration_tests/scripts/wait_for_server.py
+++ integration_tests/scripts/wait_for_server.py
@@ -5,17 +5,21 @@ import os
 
 start_time = time.time()
 
-TIMEOUT_SECONDS = 20
+TIMEOUT_SECONDS = 40
+RETRY_RATE = 0.2
 # Checks if the script should timeout
 def is_timeout():
     return (time.time() - start_time) > TIMEOUT_SECONDS
 
 while not is_timeout():
-    res = requests.get("http://localhost:9000/ready")
-    if res.status_code == 200:
-        print("Server responded with 200. Success!")
-        sys.exit(os.EX_OK)
-    time.sleep(0.2)
+    try:
+        res = requests.get("http://localhost:9000/ready", timeout=20)
+        if res.status_code == 200:
+            print("Server responded with 200. Success!")
+            sys.exit(os.EX_OK)
+    except Exception as e:
+        print("Pinging server failed. Retrying")
+    time.sleep(RETRY_RATE)
 
 print("Server failed to respond with a 200.")
 sys.exit(os.EX_OK)
