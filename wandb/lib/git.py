880b51d1e30bdfaf0fe2d4b40407b4369ac231c3
Fariz Rahman <farizrahman4u@gmail.com>
2020-08-21 08:02:56 +0530

    refac GitRepo

--- wandb/lib/git.py
+++ wandb/lib/git.py
@@ -148,7 +148,7 @@ class GitRepo(object):
             return most_recent_ancestor
         except exc.GitCommandError as e:
             logger.debug("git remote upstream fork point could not be found")
-            logger.debug(e.message)
+            logger.debug(str(e))
             return None
 
     def tag(self, name, message):
