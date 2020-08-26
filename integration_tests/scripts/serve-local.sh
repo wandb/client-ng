5256cfb836c78717d847a822a54e3e86567098ba
Nicholas Bardy <nicholasbardy@gmail.com>
2020-08-26 12:47:43 -0700

    change port for server

--- integration_tests/scripts/serve-local.sh
+++ integration_tests/scripts/serve-local.sh
@@ -4,7 +4,8 @@ VERSION=latest
 docker run --rm\
     -e CI=1\
     -e DISABLE_TELEMETRY=true\
-    -e GORILLA_FRONTEND_HOST=http://localhost:9000\
+    -e GORILLA_FRONTEND_HOST=http://localhost:8080\
     -d\
+    -p 8080:8080 -p 3306:3306 -p 8083:8083 -p 9000:9000\
     --name wandb_local \
     wandb/local:$VERSION
