6f1e32011983dd8a83b46270c11610654b78f9a7
Nicholas Bardy <nicholasbardy@gmail.com>
2020-09-03 15:15:31 -0700

    soak circle

---
+++ integration_tests/scripts/soak-circle.sh
@@ -0,0 +1,9 @@
+BRANCH=$(git branch --show-current);
+
+for run in {1..20}
+do
+curl -u ${CIRCLE_API_USER_TOKEN}: \
+     -d build_parameters[CIRCLE_JOB]=integration \
+     https://circleci.com/api/v1.1/project/github/wandb/client-ng/tree/$BRANCH
+sleep 1
+done
