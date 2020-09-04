7392a19602ce5863ec28f7cae5a1866605b203ad
Fariz Rahman <farizrahman4u@gmail.com>
2020-09-04 11:09:53 +0530

    fix blocking call

--- wandb/lib/tpu.py
+++ wandb/lib/tpu.py
@@ -1,5 +1,6 @@
 import logging
 import os
+import threading
 
 try:
     from tensorflow.python.distribute.cluster_resolver import tpu_cluster_resolver
@@ -19,7 +20,8 @@ class TPUProfiler(object):
                  service_addr=None,
                  tpu=None,
                  tpu_zone=None,
-                 gcp_project=None):
+                 gcp_project=None,
+                 duration_ms=1000):
         if service_addr:
             if tpu:
                 logger.warn("Both service_addr and tpu arguments provided. "
@@ -36,17 +38,28 @@ class TPUProfiler(object):
                                 "arguments to specify zone and project for your TPU.")
         service_addr = service_addr.replace('grpc://', '').replace(':8470', ':8466')
         self.service_addr = service_addr
+        self.duration_ms = duration_ms
         self._tpu_utilization = 0.0
+        self._thread = threading.Thread(target=self._thread_body)
+        self._thread.start()
+
+    def _get_tpu_utilization(self):
+        # this call blocks for duration_ms milliseconds
+        res = profiler_client.monitor(self.service_addr,
+                                      duration_ms=self.duration_ms,
+                                      level=2)
+        return float(res.split("Utilization ")[1].split(': ')[1].split('%')[0])
+
+    def _thread_body(self):
+        while True:
+            try:
+                self._tpu_utilization = self._get_tpu_utilization()
+            except Exception:
+                # Happens when previous profiler session is still active.
+                pass
 
     def get_tpu_utilization(self):
-        try:
-            res = profiler_client.monitor(self.service_addr, duration_ms=500, level=2)
-            val = float(res.split("Utilization ")[1].split(': ')[1].split('%')[0])
-            self._tpu_utilization = val
-            return val
-        except Exception:
-            # Happens when previous profiler session is still active.
-            return self._tpu_utilization
+        return self._tpu_utilization
 
 
 def is_tpu_available():
