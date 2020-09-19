c02dcb9fceb86a494851e4fad0ca3bde519d898d
Fariz Rahman <farizrahman4u@gmail.com>
2020-09-20 03:45:53 +0530

    colab fix

--- wandb/lib/ipython.py
+++ wandb/lib/ipython.py
@@ -2,9 +2,18 @@ def _get_python_type():
     try:
         from IPython import get_ipython
     except ImportError:
-        return "python"
+        return 'python'
 
     if get_ipython() is None:
+        # NOTE: get_ipython() returns None when called
+        # by a widget in Colab, but not in regular
+        # jupyter notebooks.
+        try:
+            import ipykernel
+        except ImportError:
+            return 'python'
+        if hasattr(ipykernel, 'zmqshell'):
+            return 'jupyter'
         return "python"
     elif 'terminal' in get_ipython().__module__:
         return 'ipython'
